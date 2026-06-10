05633dd31d3bcd27b390709e9d528436fbf47864
LEXA-14 <114798705+LEXA-14@users.noreply.github.com>
2026-06-10 19:06:45 -0400

    Feat: Crea los usescases (upsert, delete, observe y getBY

---
+++ app/src/main/java/com/example/leudy_jaquez_ap2_p1/domain/borrame/usecase/upsertUseCases.kt
@@ -0,0 +1,49 @@
+package com.example.leudy_jaquez_ap2_p1.domain.borrame.usecase
+
+import com.example.leudy_jaquez_ap2_p1.domain.borrame.model.amonestacion
+import com.example.leudy_jaquez_ap2_p1.domain.borrame.repository.borrameRepository
+import com.example.leudy_jaquez_ap2_p1.domain.borrame.validaciones.validateMonto
+import com.example.leudy_jaquez_ap2_p1.domain.borrame.validaciones.validateNombre
+import com.example.leudy_jaquez_ap2_p1.domain.borrame.validaciones.validateRazon
+import javax.inject.Inject
+
+class upsertUseCases @Inject constructor(
+    private val repository: borrameRepository
+){
+    suspend operator fun invoke(amonestacion: amonestacion): Result<Int>{
+        val nombreValido= validateNombre(amonestacion.nombres)
+        val razonValida= validateRazon(amonestacion.razon)
+        val montoValido= validateMonto(amonestacion.monto)
+
+        if(!nombreValido.isValid){
+            return Result.failure(IllegalArgumentException(nombreValido.mensaje))
+        }
+        if(!razonValida.isValid){
+            return Result.failure(IllegalArgumentException(razonValida.mensaje))
+        }
+        if(!montoValido.isValid){
+            return Result.failure(IllegalArgumentException(montoValido.mensaje))
+        }
+        return runCatching {repository.upsert(amonestacion)}
+
+    }
+}
+
+
+class getByIdUseCase @Inject constructor(
+    private val repository: borrameRepository
+){
+    suspend operator fun invoke(id: Int)=repository.getBorrame(id)
+}
+
+class deleteByIdUseCase @Inject constructor(
+    private val repository: borrameRepository
+){
+    suspend  operator fun invoke(id: Int)=repository.delete(id)
+}
+
+class observeAll @Inject constructor(
+    private val repository: borrameRepository
+){
+    suspend operator fun invoke()=repository.observeAll()
+}
